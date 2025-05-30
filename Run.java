eb0320d2444b4cc81529d3b1e9d79311013e41ba
cristian66333 <74688238+Cristian66333@users.noreply.github.com>
2025-05-30 09:03:38 -0500

    Cambios gym

--- Run.java
+++ Run.java
@@ -1,75 +1,84 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Run {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Gimnasio gimnasioUPTC = new Gimnasio();
         int opcion = 0;
+        try {
+            int division = 1/0;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"No se puede dividir entre 0");
+        }
+        
         do {
-            System.out.println("""
+            try {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog("""
                     1. Crear miembro
                     2. Mostrar miembros
                     3. Eliminar miembro por cedula
                     4. Asignar entrenador
                     5. Calcular costo membresia
                     6. Salir
-                    """);
-            opcion = sc.nextInt();
-            sc.nextLine();
+                    """));
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null,"Dato erroneo ingrese un numero");
+            }
+            
+            
             switch (opcion) {
                 case 1:
-                    System.out.println("Ingrese el nombre del usuario");
-                    String nombreUsuario = sc.nextLine();
-                    System.out.println("Ingrese el apellido");
-                    String apellidoUsuario = sc.nextLine();
-                    System.out.println("Ingrese la cedula");
-                    String cedula = sc.nextLine();
-                    System.out.println("Ingrese la edad");
-                    int edadUsuario = sc.nextInt();
-                    System.out.println("Ingrese los meses de antiguedad");
-                    int mesesAntiguedadUsuario = sc.nextInt();
-                    System.out.println("""
+                    String nombreUsuario = JOptionPane.showInputDialog("Ingrese el nombre del usuario");
+                    
+                    String apellidoUsuario = JOptionPane.showInputDialog("Ingrese el apellido");
+                    
+                    String cedula = JOptionPane.showInputDialog("Ingrese la cedula");
+                
+                    int edadUsuario = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad"));
+                    
+                    int mesesAntiguedadUsuario = Integer.parseInt(JOptionPane.showInputDialog("Ingrese los meses de antiguedad"));
+                    
+                    byte membresiaUsuario = Byte.parseByte(JOptionPane.showInputDialog("""
                             Ingrese la membresia que quiere
                             1. Bronce
                             2. Platinum
                             3. Gold
-                            """);
-                    byte membresiaUsuario = sc.nextByte();
-                    System.out.println(gimnasioUPTC.crearMiembro(nombreUsuario, apellidoUsuario, cedula, edadUsuario,
+                            """));
+                    
+                   JOptionPane.showMessageDialog(null, gimnasioUPTC.crearMiembro(nombreUsuario, apellidoUsuario, cedula, edadUsuario,
                             mesesAntiguedadUsuario, membresiaUsuario));
                     break;
                 case 2:
-                    System.out.println(gimnasioUPTC.mostrarMiembros());
+                    JOptionPane.showMessageDialog(null, gimnasioUPTC.mostrarMiembros());
                     break;
                 case 3:
-                    System.out.println("Ingrese la cedula a eliminar");
-                    String cedulaEliminar = sc.nextLine();
+                    String cedulaEliminar = JOptionPane.showInputDialog("Ingrese la cedula a eliminar");
                     if (gimnasioUPTC.eliminarUsuario(cedulaEliminar)) {
-                        System.out.println("El usuario se eliminó");
+                       JOptionPane.showMessageDialog(null,"El usuario se eliminó");
                     } else {
-                        System.out.println("El usuario no fue encontrado");
+                        JOptionPane.showMessageDialog(null,"El usuario no fue encontrado");
                     }
                     break;
                 case 4:
-                    System.out.println("Ingrese la cedula del miembro\nque desea adquirir el servicio de entrnamiento");
-                    String cedulaMiembroEntrenador = sc.nextLine();
-
+                    String cedulaMiembroEntrenador = JOptionPane.showInputDialog("Ingrese la cedula del miembro\nque desea adquirir el servicio de entrnamiento");
+                    String entrenadores = "";
                     for (int i = 0; i < gimnasioUPTC.getEntrenadores().size(); i++) {
-                        System.out.println((i + 1) + ". " + gimnasioUPTC.getEntrenadores().get(i).toString());
+                       entrenadores+=(i + 1) + ". " + gimnasioUPTC.getEntrenadores().get(i).toString()+"\n";
                     }
-                    System.out.println("Ingrese que entrenador quiere adquirir");
-                    byte seleccionEntrenador = sc.nextByte();
-                    gimnasioUPTC.asignarEntrenador(cedulaMiembroEntrenador, seleccionEntrenador);
+                    JOptionPane.showMessageDialog(null, entrenadores);
+                    byte seleccionEntrenador = Byte.parseByte(JOptionPane.showInputDialog("Ingrese que entrenador quiere adquirir"));
+                    
+                    JOptionPane.showMessageDialog(null,gimnasioUPTC.asignarEntrenador(cedulaMiembroEntrenador, seleccionEntrenador));
 
                     break;
                 case 5:
-                    System.out.println("Ingrese la cedula del miembro");
-                    String cedulaCalcularMembresia = sc.nextLine();
+                    String cedulaCalcularMembresia = JOptionPane.showInputDialog("Ingrese la cedula del miembro");
                     double resultadoPrecioMensual = gimnasioUPTC.calcularMembresia(cedulaCalcularMembresia);
                     if (resultadoPrecioMensual==-1) {
-                        System.out.println("Miembro no encontrado");
+                       JOptionPane.showMessageDialog(null,"Miembro no encontrado");
                     }else{
-                        System.out.println("El valor mensual de la membresia es " + resultadoPrecioMensual);
+                        JOptionPane.showMessageDialog(null, "El valor mensual de la membresia es " + resultadoPrecioMensual);
                     }
                     break;
 
